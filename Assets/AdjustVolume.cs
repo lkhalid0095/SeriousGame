a72aa9ad1ffc2d1f1717ea7fc86dafb83c5e5629
Lubna <lkhalid0095@bths.edu>
2021-12-19 22:34:13 -0500

    changes to settings

---
+++ Assets/AdjustVolume.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustVolume : MonoBehaviour
+{
+    public Slider volumeSlider;
+    AudioSource audioSource;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (!audioSource.playOnAwake)
+        {
+            
+            audioSource.Play();
+        }
+    }
+// Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    void OnEnable()
+    {
+        //Register Slider Events
+        volumeSlider.onValueChanged.AddListener(delegate { changeVolume(volumeSlider.value); });
+    }
+
+    //Called when Slider is moved
+    void changeVolume(float sliderValue)
+    {
+        audioSource.volume = sliderValue;
+    }
+
+}
